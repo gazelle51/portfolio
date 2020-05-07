0601213a2e83d7b98748529009bd4e86af0a99e9
gazelle51 <sarahgazelle@hotmail.com>
2020-05-07 11:03:44 +1000

    Updates

---
+++ precache-manifest.19d01722a4e2dd627ce8fc140e471715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d00a777a1620ca875c53b377d556a1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2acebddd9066565a29a5",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "108334e86d3d5be1dd85",
+    "url": "/portfolio/static/js/2.7659792e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.7659792e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2acebddd9066565a29a5",
+    "url": "/portfolio/static/js/main.f6967f07.chunk.js"
+  },
+  {
+    "revision": "8aceb13f6772a4d5a4b8",
+    "url": "/portfolio/static/js/runtime-main.7b598343.js"
+  }
+]);
